a8b37e408369bea4dffe5832e91827dd6e5b1245
KWeselski <weselski.kamil@gmail.com>
2021-02-15 09:14:45 +0100

    fix import

--- frontend/src/components/Overview/OperationsIcons/index.js
+++ frontend/src/components/Overview/OperationsIcons/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import {Grid, Tooltip} from "@material-ui/core/Grid";
+import {Grid, Tooltip} from "@material-ui/core";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
